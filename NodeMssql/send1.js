6e1443400128d04f0090e316364bf7b9d74c2ad6
AIMKK <a_5210@126.com>
2018-08-19 23:31:37 +0800

    send ajust

--- NodeMssql/send1.js
+++ NodeMssql/send1.js
@@ -5,9 +5,12 @@ var connOPtions = require('../rabitmqConnConfig.js');
 var open = require('amqplib').connect(connOPtions);
 
 open.then(function(conn) {
-    return conn.createChannel();
-}).then(function(ch) {
-    ch.assertQueue(q, { durable: false });
-    ch.sendToQueue(q, Buffer.from('something to do'));
-    console.log('ok')
+    return conn.createChannel().then(function(ch) {
+        return ch.assertQueue(q, { durable: false }).then(function() {
+            ch.sendToQueue(q, Buffer.from('something to do'));
+            console.log('ok');
+            conn.close();
+            process.exit(0)
+        });
+    });
 }).catch(console.warn);
