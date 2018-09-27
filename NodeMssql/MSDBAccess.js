13dcddf4733e66080355007e91159ba661d2b335
AIMKK <a_5210@126.com>
2018-09-27 23:16:14 +0800

    dbaccess add

--- NodeMssql/MSDBAccess.js
+++ NodeMssql/MSDBAccess.js
@@ -1,6 +1,22 @@
-const SPs = {};
-module.exports = SPs;
-//
-SPs.UserInfoGet = function(UserCode) {
+const sqlDB = require('mssql');
+module.exports = {
+    getDBConnPool,
+    getDBCommand
+};
 
+function getDBConnPool(config) {
+    var dbConnPool = sqlDB.connect(config);
+    return dbConnPool;
+}
+
+function getDBCommand(param /* [pool or transaction] */ ) {
+    var command = {};
+    var request = new sqlDB.Request(param); //param.request();
+    if (request == null) return null;
+    command.UserInfoGet = function(UserCode) {
+        var sql = 'select * from Users where UserCode = @UserCode';
+        request.input('UserCode', sqlDB.VarChar, UserCode);
+        return request.query(sql);
+    }
+    return command;
 }
---
+++ NodeMssql/dbtest2.js
@@ -0,0 +1,40 @@
+const dbAccess = require('./MSDBAccess.js')
+
+const config = {
+    user: 'sa',
+    password: '123',
+    server: '127.0.0.1', // You can use 'localhost\\instance' to connect to named instance
+    database: 'tediousTestDB',
+
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+}
+
+//
+var dbconnPool = dbAccess.getDBConnPool(config);
+dbconnPool.then(pool => {
+    var trans = pool.transaction();
+    var dbCommand = dbAccess.getDBCommand(trans);
+    trans.begin()
+        .then(() => {
+            dbCommand.UserInfoGet('0000001')
+                .then((result) => {
+                    console.dir(result);
+                })
+        })
+        .then(() => {
+            trans.commit(err => {
+                // ... error checks
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+            trans.rollback(err => {
+                // ... error checks
+            })
+        })
+}).catch((error) => {
+    console.log(error);
+});
+//
