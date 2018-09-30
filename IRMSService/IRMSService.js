0b04c571e45202aac715f88eb4af80fb9078a3a4
AIMKK <a_5210@126.com>
2018-09-30 11:34:28 +0800

    router and  static resource

--- IRMSService/IRMSService.js
+++ IRMSService/IRMSService.js
@@ -1,31 +1,66 @@
 const Koa = require('koa');
+const path = require('path');
+const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+const static = require('koa-static');
+
+const staticPath = './static';
+//
 const app = new Koa();
-
-app.use(async(ctx) => {
-    //if get method response user form
-    if (ctx.url === '/' && ctx.method.toLowerCase() === 'get') {
-        let html = `
-        <h1>Koa2 request post demo</h1>
-        <form method="POST"  action="/">
-            <p>userName</p>
-            <input name="userName" /> <br/>
-            <p>age</p>
-            <input name="age" /> <br/>
-            <p>webSite</p>
-            <input name='webSite' /><br/>
-            <button type="submit">submit</button>
-        </form>
-        `;
-        ctx.body = html;
-    } else if (ctx.url === '/' && ctx.method.toLowerCase() === 'post') {
-        let pastData = await parsePostData(ctx);
-        ctx.body = pastData;
-    } else {
-        //其它请求显示404页面
-        ctx.body = '<h1>404!</h1>';
-    }
-
+const router = new Router({
+    prefix: ''
 });
+
+const home = new Router();
+home.get('/hello', async(ctx) => {
+    ctx.body = ctx.query;
+}).get('/todo', async(ctx) => {
+    ctx.body = ctx.query;
+})
+const page = new Router();
+page.get('/hello', async(ctx) => {
+    ctx.body = 'page hello'
+}).get('/todo', async(ctx) => {
+    ctx.body = 'page todo'
+})
+
+//
+router.use('/home', home.routes(), home.allowedMethods());
+router.use('/page', page.routes(), page.allowedMethods());
+//
+app.use(static(path.join(__dirname, staticPath)));
+//
+app.use(router.routes())
+    .use(router.allowedMethods()).use(bodyParser());
+
+// //
+// app.use(async(ctx) => {
+//     //if get method response user form
+//     if (ctx.url === '/' && ctx.method.toLowerCase() === 'get') {
+//         let html = `
+//         <h1>Koa2 request post demo</h1>
+//         <form method="POST"  action="/">
+//             <p>userName</p>
+//             <input name="userName" /> <br/>
+//             <p>age</p>
+//             <input name="age" /> <br/>
+//             <p>webSite</p>
+//             <input name='webSite' /><br/>
+//             <button type="submit">submit</button>
+//         </form>
+//         `;
+//         ctx.body = html;
+//     } else if (ctx.url === '/' && ctx.method.toLowerCase() === 'post') {
+//         // let postData = await parsePostData(ctx);
+//         // ctx.body = postData;
+//         let postData = ctx.request.body;
+//         ctx.body = postData;
+//     } else {
+//         //其它请求显示404页面
+//         ctx.body = '<h1>404!</h1>';
+//     }
+
+// });
 app.listen(3000);
 console.log('server start at 3000')
     //
