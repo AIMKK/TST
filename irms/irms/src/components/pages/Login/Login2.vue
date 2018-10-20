fb1ffd11e16867860fd106bfad096cfc324ffb52
AIMKK <a_5210@126.com>
2018-10-20 15:24:58 +0800

    login

---
+++ irms/irms/src/components/pages/Login/Login2.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login-container">
+        <div class="username">
+            <input type="text">
+        </div>
+        <div class="password">
+
+        </div>
+        <div class="location">
+
+        </div>
+        <div class="login-button">
+            
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+    }
+</script>
+<style scoped>
+
+</style>
