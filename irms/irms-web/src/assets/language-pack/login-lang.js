ba0ecb94e09d6b6120e86423a127bf6f09c4964f
AIMKK <a_5210@126.com>
2018-12-14 16:16:30 +0800

    vueexpore

---
+++ irms/irms-web/src/assets/language-pack/login-lang.js
@@ -0,0 +1,12 @@
+var loginLang={
+    login:{
+        CHS:"登录",
+        CHI:"登錄",
+        ENG:"login",
+    },
+    title:{
+        CHS:"iRMS",
+        CHI:"iRMS",
+        ENG:"iRMS",
+    },
+}
